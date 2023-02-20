7713545752cc646d37cdc9e6cd242112c47a403b
Ksenia Starchenko <starchenko.ksenia@gmail.com>
2023-02-20 18:16:24 +0400

    end code

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int account = 324;
+        int deposit = 1126;
+
+        int bonus;
+        if (deposit > 1000) {
+            bonus = deposit / 100;
+
+        }    else {
+                    bonus = 0;
+        }
+
+        System.out.println("Итоговая сумма = ");
+        System.out.println(account + deposit + bonus);
+        System.out.println("Количество бонусов =" );
+        System.out.println(bonus);
+
+        }
+
+    }
+
+
